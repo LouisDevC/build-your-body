e87a44700884424c54c1720dba8a514fb61d2b98
Devlouis_C <DevLouisC@outlook.com>
2024-09-19 07:50:42 -0300

    estrutura e estilizações
    criado a estrutura base e os estilos, iniciado a logica do formulário

---
+++ src/scripts/script.js
@@ -0,0 +1,37 @@
+// show and hide do formulario
+// variáveis
+const newExerciseBtn = document.querySelector('#new-btn')
+const formContainer = document.querySelector('.informations')
+const closeForm = document.querySelector('#close')
+// getform
+const exerciseInput = document.querySelector('#ex-input')
+const choiceSerie = document.querySelector('#serie-options')
+const serie = document.querySelector('#series')
+const newSerieInput = document.querySelector('serie-input')
+const day = document.querySelector('#day-select')
+// funções
+
+// eventos
+newExerciseBtn.addEventListener('click', () =>{
+    newExerciseBtn.textContent = 'Inserir treino!'
+    formContainer.classList.remove("hide")
+
+    if(newExerciseBtn.textContent === 'Inserir treino!'){
+        newExerciseBtn.addEventListener('click', () =>{
+            const exercise = exerciseInput.value
+            const SerieType = choiceSerie.value
+            const dayTraining = day.value
+            const valueInSerieOptions = choiceSerie.value
+            const valueInOptions = serie.value
+            
+            if (!exercise) return
+
+            serie.classList.remove('hide')
+        })
+    }
+})
+
+closeForm.addEventListener('click', () =>{
+    formContainer.classList.add("hide")
+    newExerciseBtn.textContent = 'Novo exercício'
+})
